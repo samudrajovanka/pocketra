323d61e8f16ca271b7d72c7f88563dfda196bd9e
Jovanka <samudrajovanka20@gmail.com>
2026-03-10 12:26:48 +0700

    fix: icons pwa and workbox

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -28,6 +28,25 @@ const config = defineConfig({
     viteReact(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
       pwaAssets: {
         disabled: false,
         config: true,
@@ -53,6 +72,17 @@ const config = defineConfig({
           {
             src: '/icons/pwa-512x512.png',
             sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icons/apple-touch-icon-180x180.png',
+            sizes: '180x180',
             type: 'image/png',
           },
         ],
